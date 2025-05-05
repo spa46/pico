9d5f82e2157f0d2bb45e2dc9f4395e3014e7ae31
seanpark <psckyky@hotmail.com>
2025-05-05 14:37:23 +0900

    [prototype] typo error

--- app/adc.py
+++ app/adc.py
@@ -9,9 +9,9 @@ class Mq135:
     def __init__(self) -> None:
         self.mq135 = MQ135(mq135)
 
-    def measure(self, temperature, humidify):
+    def measure(self, temperature, humidity):
         rzero = self.mq135.get_rzero()
-        corrected_rzero = self.mq135.get_corrected_rzero(temperature, humidify)
+        corrected_rzero = self.mq135.get_corrected_rzero(temperature, humidity)
         resistance = self.mq135.get_resistance()
         ppm = self.mq135.get_ppm()
         corrected_ppm = self.mq135.get_corrected_ppm(temperature, humidity)
